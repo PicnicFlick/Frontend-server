3dac19ce69c72f283f7ae68addd4136e4045d911
노준호 <wnsgh1873@gmail.com>
2024-04-03 18:32:02 +0900

    qr 연결 시도

--- src/components/QrScanner.js
+++ src/components/QrScanner.js
@@ -1,12 +1,21 @@
 import { Scanner } from "@yudiel/react-qr-scanner";
-import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+//@yudiel/react-qr/scanner 사랑해요 => 지피티로 최신 라이브러리 물어보고 공식문서 적극 활용
 
 function QrScanner() {
+    const navigate=useNavigate();
+    // const [urlText,setUrlText]=useState("");
 
+    // useEffect(()=>{
+    //     if(urlText.length>0)
+    //         (urlText);
+    // },[urlText])
     return (
         <Scanner
             onResult={(text, result) => {
-                console.log("text, result:", text, result)
+                console.log("text, result:", text, result);
+                navigate(text);
             }
             }
             onError={(error) => {

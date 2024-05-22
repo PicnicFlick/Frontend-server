1d0e35bec3a3be9909155d1df4990adfb7e9c1e3
Kimjuyoung <jjassb404@naver.com>
2024-05-22 19:26:47 +0900

    style : 관리자 history index 셋팅

--- src/pages/Auth/Auth_History.js
+++ src/pages/Auth/Auth_History.js
@@ -18,9 +18,9 @@ function Auth_History(){
     const fetchHistory = async() => {
         try{
             const URL = process.env.REACT_APP_BACK_API;
-            // const token = tmpToken;
-            const token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMzgxNDE0MTc0IiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTcxNjU5NDU5M30.UptgLHZq6XBVwzvtO3Q-hUEnPrLi9Nsac9dGSMOB4HI";
-            console.log('token!!!! : ',token);
+            const token = sessionStorage.getItem('token');
+            // const token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzMzgxNDE0MTc0IiwiYXV0aCI6IlJPTEVfVVNFUiIsImV4cCI6MTcxNjU5NDU5M30.UptgLHZq6XBVwzvtO3Q-hUEnPrLi9Nsac9dGSMOB4HI";
+            console.log('token : ',token);
             //[관리자]전체 사용자 내역 조회 API
             const response = await axios.get(`${URL}/api/v1/history/admin`,{
                 headers:{
@@ -31,13 +31,17 @@ function Auth_History(){
                     status:'RETURNED'
                 }
             });
-            setHistoryList(response.data.result.historyList);
-            setPageNum(response.data.result.pageNum);
-            setTotalCnt(response.data.result.totalCnt);
-            setTotalPages(response.data.result.totalPages);
-            //response.data.result에 있는 모든 속성들 변수화
-
             console.log(response.data);
+            const result = response.data.result;
+            if(result){
+                setHistoryList(response.data.result.historyList);
+                setPageNum(response.data.result.pageNum);
+                setTotalCnt(response.data.result.totalCnt);
+                setTotalPages(response.data.result.totalPages);
+                //response.data.result에 있는 모든 속성들 변수화
+            } else {
+                console.error('Unexpected response structure:', response.data);
+            }
         }catch(error){
             alert(error);
         }
@@ -67,9 +71,17 @@ function Auth_History(){
                 <Date2>날짜 모달</Date2>
             </Date>
             <FullBox>
+            <IndexBox>
+                <IndexTopic>대여 ID</IndexTopic>
+                <IndexTopic>대여자 이름</IndexTopic>
+                <IndexTopic>E-mail</IndexTopic>
+                <IndexTopic>대여 수량</IndexTopic>
+                <IndexTopic>대여 일시</IndexTopic>
+                <IndexTopic>결제금액</IndexTopic>
+                <IndexTopic>대여상태</IndexTopic>
+                <IndexTopic>비고</IndexTopic>
 
-            <div>주영이가 작업할 곳 1</div>
-            <div>HistoryList</div>
+            </IndexBox>
             {historyList.map((item,index)=>
             <h1>{item.started_time} / {item.returned_time}</h1>)}
             </FullBox>
@@ -120,12 +132,39 @@ margin-left:10px;
 width: 947px;
 height: 418px;
 border-radius:15px;
-background-color: pink;
+border: 5px solid #7D7D7D;
+background-color: transparent;/*투명색!*/
 `;
-const Index = styled.div``;
-const Index_Text = styled.text``;
-const AllBox = styled.div``;
-const OneBox = styled.div``;
+const IndexBox = styled.div`
+display: flex;
+flex-direction: row;
+margin-right: 1px;
+margin-left: 10px;
+margin-top: 15px;
+margin-bottom: 30px;
+border-bottom: 3px solid #D9D9D9; /* 줄 */
+`;
+const IndexTopic = styled.h1`
+    width: 86px;
+    height: 33px;
+    flex-shrink: 0;
+    color: var(--kakao-logo, #000);
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 24px; /* 133.333% */
+    letter-spacing: -0.333px;
+    margin-right:20px;
+    margin-left:10px;
+`;
+const AllBox = styled.div`
+
+`;
+const OneBox = styled.div`
+
+`;
 //버튼
 const Button_MoreDetail = styled.button``;
 const Button_Delete = styled.button``;

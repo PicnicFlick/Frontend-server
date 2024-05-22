5d023bbfb3ea75e000f382496147963200c4f8e6
Kimjuyoung <jjassb404@naver.com>
2024-05-22 17:38:46 +0900

    fix : 파일 날려먹은거 프로젝트 새로 팜...근데 main에 수정함

--- src/pages/Auth/Auth_History.js
+++ src/pages/Auth/Auth_History.js
@@ -59,25 +59,69 @@ function Auth_History(){
             }
     },[])
 
-
     return (
         <Wrapper_Auth>
             <Auth_Header text={text}/>
+            <Date>
+                <Date1>일자</Date1>
+                <Date2>날짜 모달</Date2>
+            </Date>
+            <FullBox>
+
             <div>주영이가 작업할 곳 1</div>
             <div>HistoryList</div>
             {historyList.map((item,index)=>
             <h1>{item.started_time} / {item.returned_time}</h1>)}
+            </FullBox>
+      
         </Wrapper_Auth>
     )
 }
 
 export default Auth_History
 //상단 일자 component
-const Date = styled.div``;
-const Date1 =styled.h1``;
-const Date2 = styled.div``;
+const Date = styled.div`
+    display:flex; /*flexbox*/
+    flex-direction:row; /*가로로 나열*/
+    margin-top:20px;
+    margin-bottom:20px;
+    margin-right:10px;
+`;
+const Date1 =styled.h1`
+width: 181px;
+height: 43px;
+flex-shrink: 0;
+color: var(--kakao-logo, #000);
+font-family: Pretendard;
+font-size: 20px;
+font-style: normal;
+font-weight: 500;
+line-height: 24px; /* 120% */
+letter-spacing: -0.333px;
+`;
+const Date2 = styled.div`
+width: 181px;
+height: 43px;
+flex-shrink: 0;
+color: var(--kakao-logo, #000);
+font-family: Pretendard;
+font-size: 20px;
+font-style: normal;
+font-weight: 500;
+line-height: 24px; /* 120% */
+letter-spacing: -0.333px;
+`;
 //히스토리 Box
-const FullBox = styled.div``;
+const FullBox = styled.div`
+margin-top:10px;
+margin-bottom:10px;
+margin-right:10px;
+margin-left:10px;
+width: 947px;
+height: 418px;
+border-radius:15px;
+background-color: pink;
+`;
 const Index = styled.div``;
 const Index_Text = styled.text``;
 const AllBox = styled.div``;

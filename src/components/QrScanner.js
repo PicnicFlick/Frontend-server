2cedd2cea7d301404dcd6f0d185028392120ffdc
노준호 <wnsgh1873@gmail.com>
2024-06-13 17:35:39 +0900

    qr scanner 해결 4

--- src/components/QrScanner.js
+++ src/components/QrScanner.js
@@ -5,6 +5,7 @@ function QrScanner() {
     return (
         <Scanner
             onResult={(text, result) => {
+                console.log('1');
                 console.log("text, result:", text, result);
                 sessionStorage.setItem('matId',text);
                 window.location.href="1";

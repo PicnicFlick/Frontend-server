0e2bb109406e4c503cddbdf644c4ae6a3f9c2c96
노준호 <wnsgh1873@gmail.com>
2024-06-13 17:14:50 +0900

    qr test 고치기 1

--- src/components/QrScanner.js
+++ src/components/QrScanner.js
@@ -2,16 +2,13 @@ import { Scanner } from "@yudiel/react-qr-scanner";
 import { useNavigate } from "react-router-dom";
 
 function QrScanner() {
-    const navigate = useNavigate();
 
     return (
         <Scanner
             onResult={(text, result) => {
                 console.log("text, result:", text, result);
-                if (text) {
-                    sessionStorage.setItem('matId',text);
-                    window.location.href='1';
-                }
+                sessionStorage.setItem('matId',text);
+                window.location.href="1";
             }}
             onError={(error) => {
                 console.log(error?.message);
